8662bde3acdc35c89b2d18c0469d45d3c5bb8a2d
Greg Moore <gsmoore@usf.edu>
2020-08-12 21:51:23 -0400

    Gallery Test

--- js/gallery-grid.js
+++ js/gallery-grid.js
@@ -1,7 +1,7 @@
 function resizeGridItem(item){
     grid = document.getElementsByClassName("gallery-grid")[0];
     rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
-    rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+    rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-gap'));
     rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
       item.style.gridRowEnd = "span "+rowSpan;
   }
